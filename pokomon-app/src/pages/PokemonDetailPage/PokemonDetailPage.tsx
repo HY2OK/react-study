fabe93194a24e342290de4a1881f0fe54aa40220
jun_ <gurwns232@naver.com>
2024-03-26 20:46:30 +0900

    feat: 상세페이지 뒤로가기 추가

--- pokomon-app/src/pages/PokemonDetailPage/PokemonDetailPage.tsx
+++ pokomon-app/src/pages/PokemonDetailPage/PokemonDetailPage.tsx
@@ -1,15 +1,23 @@
-import { useLocation, useParams } from 'react-router-dom'
+import { useLocation, useNavigate, useParams } from 'react-router-dom'
 import { useGetPokemonDetailsByNameQuery } from '../../redux/services/pokemon'
 import PokemonDetail from '../../components/PokemonDetail/PokemonDetail'
+import { FaArrowLeft } from 'react-icons/fa'
 
 const PokemonDetailPage = () => {
   const { name } = useParams()
   const location = useLocation()
+  const navigate = useNavigate()
   const korName = new URLSearchParams(location.search).get('kor') || null
   const { data, error, isLoading } = useGetPokemonDetailsByNameQuery(name!)
 
   return (
-    <div className="w-[80%] h-[750px] bg-white border-8 border-slate-800 rounded-xl flex justify-center items-center ">
+    <div className="w-[80%] h-[750px] bg-white border-8 border-slate-800 rounded-xl flex justify-center items-center relative ">
+      <div
+        className="absolute top-8 left-8 text-3xl cursor-pointer z-10"
+        onClick={() => navigate(-1)}
+      >
+        <FaArrowLeft />
+      </div>
       {error && <div>error</div>}
       {isLoading && <div>loading...</div>}
       {data && <PokemonDetail data={data} korName={korName!} />}
